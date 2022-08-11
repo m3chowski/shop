cd5b1d3d746f216565747e5c1f64b2b3d4edb4dd
M3chowski <k4rrrlson@gmail.com>
2022-08-11 13:43:27 +0400

    add order page

--- src/components/order-information/Order-Information.js
+++ src/components/order-information/Order-Information.js
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
-import { Button, ButtonGroup } from "@mui/material";
+import {
+  Button,
+  ButtonGroup,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import "./order-information.css";
 
 export const OrderInformation = () => {
-  const [activeDelivery, setActiveDelivery] = useState("delivery");
-
   const delivery = ["delivery", "pickup"];
+  const personalData = ["First name", "Last name", "Email", "Mobile number"];
+  const deliveryInfo = [
+    "Country",
+    "Address",
+    "Postal code",
+    "District",
+    "Province",
+  ];
+  const pickupPoint = ["first pickup", "second pickup"];
+  const paymentMethod = ["card payment", "cash payment"];
 
+  const [activeDelivery, setActiveDelivery] = useState("delivery");
+  const [activePaymentMethod, setActivePaymentMethod] = useState("");
   return (
     <div className="order">
-      <div>
-        <ButtonGroup color="inherit">
+      <div className="order-delivery">
+        <ButtonGroup fullWidth color="inherit">
           {delivery.map((item) => (
             <Button
               onClick={() => setActiveDelivery(item)}
@@ -27,7 +45,50 @@ export const OrderInformation = () => {
           ))}
         </ButtonGroup>
       </div>
-      <div></div>
+      <div className="order-name">
+        {personalData.map((title) => (
+          <TextField label={title} />
+        ))}
+      </div>
+      <div className="order-delivery-address">
+        {activeDelivery !== "pickup" ? (
+          <div className="order-name">
+            {deliveryInfo.map((title) => (
+              <TextField label={title} />
+            ))}
+          </div>
+        ) : (
+          <FormControl fullWidth>
+            <InputLabel>Pickup point</InputLabel>
+            <Select label="pickup point">
+              {pickupPoint.map((title, index) => (
+                <MenuItem value={index}>{title}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        )}
+      </div>
+      <div className="order-payment-method">
+        <h4>Payment method</h4>
+        {
+          <ButtonGroup fullWidth color="inherit">
+            {paymentMethod.map((title) => (
+              <Button
+                onClick={() => setActivePaymentMethod(title)}
+                sx={
+                  activePaymentMethod === title
+                    ? {
+                        color: "white",
+                      }
+                    : { color: "#808080" }
+                }
+              >
+                {title}
+              </Button>
+            ))}
+          </ButtonGroup>
+        }
+      </div>
     </div>
   );
 };
--- src/pages/order/Order.js
+++ src/pages/order/Order.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { OrderInformation } from "../../components/order-information/Order-Information";
+import { OrderInformation } from "../../components/order-information";
+import { OrderItems } from "../../components/order-items/Order-Items";
 
 import "./order.css";
 
 export const Order = () => {
   return (
-    <div>
+    <div className="order-page">
       <OrderInformation />
+      <OrderItems />
     </div>
   );
 };
